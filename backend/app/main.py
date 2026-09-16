8dba4bbb18b285a82c91991d5d0cd6c3080865b9
ai-labs-uc <ai-labs@uc-bcf.edu.ph>
2026-09-16 18:30:15 +0800

    Add files via upload

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,32 +2,53 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.routes import router
-from app.api.auth_routes import router as auth_router
-from app.core.config import settings
+
 
 app = FastAPI(
-    title=settings.app_name,
-    version="1.0.0-simple",
+    title="University Research Call for Paper Opportunity and Grants",
+    version="1.0.0-simple"
 )
+
+
 
 app.add_middleware(
     CORSMiddleware,
+
     allow_origins=[
-        item.strip()
-        for item in settings.cors_origins.split(",")
+        "*"
     ],
+
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+
+    allow_methods=[
+        "*"
+    ],
+
+    allow_headers=[
+        "*"
+    ],
 )
 
-app.include_router(auth_router)
-app.include_router(router)
+
+
+app.include_router(
+    router,
+    prefix="/api"
+)
+
+
 
 @app.get("/health")
 def health():
+
     return {
+
         "status": "ok",
-        "system": settings.app_name,
-        "version": "1.0.0-simple",
+
+        "system":
+        "University Research Call for Paper Opportunity and Grants",
+
+        "version":
+        "1.0.0-simple"
+
     }
